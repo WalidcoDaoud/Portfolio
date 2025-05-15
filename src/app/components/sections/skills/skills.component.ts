434cb0e93626ace77dd2510f36343e98c4522bf5
Walid <walidkd208@gmail.com>
2025-05-14 22:27:05 -0300

    feature: techincal skills implemented

--- src/app/components/sections/skills/skills.component.ts
+++ src/app/components/sections/skills/skills.component.ts
@@ -1,11 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-skills',
-  imports: [],
+  imports: [CommonModule],
+  standalone: true,
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.sass'
 })
 export class SkillsComponent {
-
+  technicalSkills = [
+    { name: 'HTML & CSS', level: 100 },
+    { name: 'JavaScript', level: 100 },
+    { name: 'TypeScript', level: 90 },
+    { name: 'React', level: 90 },
+    { name: 'Angular', level: 90 },
+    { name: 'C#', level: 60 },
+    { name: 'Sass / SCSS', level: 100},
+    { name: 'Material UI', level: 70},
+    { name: 'Node.js', level: 50},
+    { name: 'Bootstrap', level: 70},
+    { name: 'RESTful APIs', level: 70},
+    { name: 'SQL Server Management Studio', level: 70},
+    { name: 'MongoDB', level: 70},
+    { name: 'Azure', level: 70},
+    { name: 'CI/CD', level: 80},
+  ];
 }
